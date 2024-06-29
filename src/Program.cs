e47094ed75a296a6b4ca1db6261da6b266565c5d
Brad Irby <brad@bradirby.com>
2024-06-29 17:33:28 +0200

    adding local storage and session storage

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,8 @@
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using BlazorDIExamples.Services;
+using Blazored.LocalStorage;
+using Blazored.SessionStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -14,6 +17,28 @@ namespace BlazorDIExamples
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddBlazoredSessionStorage(config => {
+                    config.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+                    config.JsonSerializerOptions.IgnoreNullValues = true;
+                    config.JsonSerializerOptions.IgnoreReadOnlyProperties = true;
+                    config.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+                    config.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                    config.JsonSerializerOptions.ReadCommentHandling = JsonCommentHandling.Skip;
+                    config.JsonSerializerOptions.WriteIndented = false;
+                }
+            );
+
+            builder.Services.AddBlazoredLocalStorage(config =>
+            {
+                config.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+                config.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+                config.JsonSerializerOptions.IgnoreReadOnlyProperties = true;
+                config.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+                config.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                config.JsonSerializerOptions.ReadCommentHandling = JsonCommentHandling.Skip;
+                config.JsonSerializerOptions.WriteIndented = false;
+            });
 
             builder.Services.AddScoped<ScopedService>();
             builder.Services.AddTransient<TransientService>();
